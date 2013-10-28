a0a6bb0e2e0507151b760a6140f2c666df5b14c5
Mathijs Baaijens <mathijs@brewinapps.com>
2013-10-28 02:25:57 +0100

    Removed incorrect parenthesis in assembler output for marker conditions.

--- Stage2/PrettyPrint.hs
+++ Stage2/PrettyPrint.hs
@@ -8,7 +8,7 @@ module Stage2.PrettyPrint (
 import Data.Char (toUpper)
 
 import Stage2.Base (AInstruction(..), ADest(..), Assembler(..))
-import Common.Simulator (LeftOrRight(..), Instruction(..))
+import Common.Simulator (LeftOrRight(..), Instruction(..), Condition(..))
 
 assemblerToString :: Assembler -> String
 assemblerToString = ats . aInstrs
@@ -19,7 +19,7 @@ assemblerToString = ats . aInstrs
 
 instructionToString :: AInstruction -> String
 instructionToString i = map toUpper . unwords $ case i of
-  ASense dir cond dest -> ["SENSE", show dir, show cond,
+  ASense dir cond dest -> ["SENSE", show dir, condToString cond,
                            "OR", destToString dest]
   AMark num            -> ["MARK", show num]
   AUnmark num          -> ["UNMARK", show num]
@@ -35,7 +35,11 @@ instructionToString i = map toUpper . unwords $ case i of
 instructionToString2 :: Instruction -> String
 instructionToString2 (Turn IsLeft state) = "Turn Left " ++ show state
 instructionToString2 (Turn IsRight state) = "Turn Right " ++ show state
+instructionToString2 (Sense dir s1 s2 c) = "Sense " ++ show dir ++ " " ++ show s1 ++ " " ++ show s2 ++ " " ++ condToString c
 instructionToString2 instruction = show instruction
+
+condToString (Marker num) = "Marker " ++ show num
+condToString c = show c
 
 dirToString IsLeft = "LEFT"
 dirToString IsRight = "RIGHT"
