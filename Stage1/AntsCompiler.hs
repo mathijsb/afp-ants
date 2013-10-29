bfdfee96258ebff1f78af50106e97f719de6be8b
Mathijs Baaijens <mathijs@brewinapps.com>
2013-10-29 21:13:00 +0100

    Further work on rewriting the compiler.

--- Stage1/AntsCompiler.hs
+++ Stage1/AntsCompiler.hs
@@ -7,14 +7,11 @@ module Stage1.AntsCompiler
 import qualified Data.Map as M
 import Data.Maybe
 import Data.List
+import Control.Monad.State
+import Control.Monad
 import Stage1.AntsBase
 import Stage2.Base
 import Debug.Trace
-
---type FunctionLookup = FuncName -> ([VarName], ProgramFlow -> [AInstruction])
---type Environment = M.Map VarName Int
---type ProgramFlow = (ADest, Label, Maybe Label, Environment)
---type LabeledFunction = (FuncName, ([VarName], ProgramFlow -> [AInstruction]))
 
 type Environment = M.Map VarName Int
 
@@ -27,7 +24,7 @@ data CompilerState = CompilerState {
 
 }
 
-type Generator = CompilerState -> [AInstruction]
+type Generator = CompilerState -> (State Int [AInstruction])
 type LabeledFunction = (FuncName, ([VarName], Generator))
 
 antsAlgebra :: AntsAlgebra [AInstruction] 
@@ -43,23 +40,46 @@ antsAlgebra = (compileProgram,
 			   (compileExpressionCommand, compileExpressionNot, compileExpressionBool, 
 			   	compileExpressionFunctionCall, compileExpressionAnd, compileExpressionOr, 
 			   	compileExpressionEquals),
-			   compileExpressionCommand)
+			   compileCommand)
 
 	where
 
 		compileProgram funcs = 
-			let (_, gen) = resolveFunction funcs "main"
-			in gen $ CompilerState funcs M.empty (ARelative 1) Nothing ""
+			let initialState = CompilerState funcs M.empty (ARelative 1) Nothing ""
+			    (_, mainGen) = resolveFunction funcs "main"
+			    mainCode = fst . runState (mainGen initialState) $ 0
+			in [ALabel1 "START"] ++ mainCode ++ [AGoto "START"]
 
 		compileFunction ident decls sts = (ident, (decls, mergeGenerators sts))
 
-		compileStatementIf expr sts1 sts2 st = undefined
-		compileStatementWhile = undefined
-		compileStatementBreak = undefined 
-		compileStatementExpr = undefined 
-		compileStatementTimes = undefined
+		compileStatementIf expr sts1 sts2 st = do
+			(labelElse, labelEnd) <- uniqueLabelTup ("ELSE", "END")
+			expressionInstructions <- expr st {failDestination = (ALabel labelElse)}
+			ifInstructions <- mergeGenerators sts1 st
+			elseInstructions <- mergeGenerators sts2 st
+			case expressionInstructions of
+				[] -> return ifInstructions
+				otherwise -> return . concat $ [expressionInstructions, ifInstructions, 
+					[AGoto labelEnd, ALabel1 labelElse], elseInstructions, [ALabel1 labelEnd]]
 
-		compileExpressionCommand = undefined
+		compileStatementWhile expr sts st = do
+			(labelWhile, labelEnd) <- uniqueLabelTup ("WHILE", "END")			
+			expressionInstructions <- expr st {failDestination = (ALabel labelEnd)}
+			whileInstructions <- mergeGenerators sts st
+			return . concat $ [[ALabel1 labelWhile], expressionInstructions, whileInstructions, 
+				[AGoto labelWhile, ALabel1 labelEnd]]
+
+		compileStatementBreak st = case breakDestination st of 
+			Just x -> return [AGoto x]
+			Nothing -> error "Break without an enclosing while statement"
+
+		compileStatementExpr = id 
+		compileStatementTimes var times sts st = liftM concat . sequence . map compile $ range
+			where
+				range = [1 .. (resolveVar times st)]
+				compile n = mergeGenerators sts st {environment = M.insert var n (environment st)}
+
+		compileExpressionCommand = id 
 		compileExpressionNot = undefined
 		compileExpressionBool = undefined
 		compileExpressionFunctionCall = undefined 
@@ -67,13 +87,31 @@ antsAlgebra = (compileProgram,
 		compileExpressionOr = undefined 
 		compileExpressionEquals = undefined
 
+		compileCommand Move st = return $ [AMove . failDestination $ st]
+		compileCommand (Sense direction condition) st = case condition of
+			MarkerVar n -> return [ASense direction (Marker ((environment st) M.! n)) (failDestination st)]
+			otherwise -> return [ASense direction condition (failDestination st)]
+
+		compileCommand (Mark num) _ = return [AMark num]
+		compileCommand (Unmark num) _ = return [AUnmark num]
+		compileCommand PickUp st = return [APickUp (failDestination st)]
+		compileCommand Drop _ = return [ADrop]
+		compileCommand (Turn dir) _ = return [ATurn dir]
+		compileCommand (Flip num) st = return [AFlip num (failDestination st)]
+		compileCommand Nop _ = return [ANop]
+
 		resolveFunction funcs name = case lookup name funcs of
 			Just f -> f
 			Nothing -> error $ "function '" ++ name ++ "' is invoked but not defined."
 
-		mergeGenerators gens st = concat . map ($st) $ gens
+		resolveVar (Var name) st = (environment st) M.! name
+		resolveVar (Value val) st = val
 
-			   
+		mergeGenerators gens st = liftM concat . sequence . map ($st) $ gens
+
+		uniqueLabel label = get >>= (\x -> put (x + 1) >> return (label ++ "_" ++ show x))
+		uniqueLabelTup (l1, l2) = get >>= (\x -> put (x + 1) >> return (l1 ++ "_" ++ show x, l2 ++ "_" ++ show x))
+
 {-
 		compileProgram instrs = [ALabel1 "START"] ++ (((snd . lookupFunction) "main") (ARelative 1, "", Nothing, M.empty)) ++ [AGoto "START"]
 			where lookupFunction name = case lookup name . map ($lookupFunction) $ instrs of
@@ -116,6 +154,7 @@ antsAlgebra = (compileProgram,
 		compileExpressionCommand = id
 		compileExpressionNot expr f (flow, context, brk, env) = (expr f (ALabel (context ++ "_NOTEND"), context, brk, env)) ++ [jumpOrGoto flow] ++ [ALabel1 (context ++ "_NOTEND")]
 		compileExpressionBool bool _ _ = []
+		
 		compileExpressionFunctionCall name vars f (flow, context, brk, env) = cont (flow, context, brk, M.union functionEnvironment env)
 			where
 				functionEnvironment = M.fromList $ zipWith (,) decls (map resolveVar vars)
